8b250d008f5aaf5f1d9561f021469f505456673d
Gabriel <cornelio.cretu@gmail.com>
2024-04-09 14:11:23 +0200

    09/04/2024 Entornos 2

--- OneDrive/Escritorio/PROGAM/Ejercicios/arraysbi/IDEGIT2/src/idegit2/IDEGIT2.java
+++ OneDrive/Escritorio/PROGAM/Ejercicios/arraysbi/IDEGIT2/src/idegit2/IDEGIT2.java
@@ -1,11 +1,9 @@
 package idegit2;
 public class IDEGIT2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Adios  mundo");
     }
     
 }
